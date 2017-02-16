a1d67a9c383909250623c08fec5dcdaebce55a71
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-16 02:17:07 +0100

    resplit test/dev/train

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -9,7 +9,7 @@ cat cs_cltt-ud-{test,dev,train}.conllu | udapy -s $SCENARIO > cs_cltt-ud-all.con
 cat cs_cltt-ud-all.conllu | udapy -HAMC ud.MarkBugs > bugs.html
 
 # test/dev/train split
-csplit cs_cltt-ud-all.conllu '/sent_id = zakon.iso-003-p23s3/' '/sent_id = vyhlaska.iso-001-p1s1/'
+csplit cs_cltt-ud-all.conllu '/sent_id = vyhlaska.iso-001-p8s1/' '/sent_id = vyhlaska.iso-006-p63s1/'
 mv xx00 ../cs_cltt-ud-test.conllu
 mv xx01 ../cs_cltt-ud-dev.conllu
 mv xx02 ../cs_cltt-ud-train.conllu
